49e82f39be67b01eb93bdc6df2a1c14f6b5a7bf4
Nasr Elsisi <nasrelsisi00@gmail.com>
2022-07-23 17:03:27 +0000

    Finish nested relationships

--- blog/api/views.py
+++ blog/api/views.py
@@ -13,14 +13,12 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+
 
 
 class UserDetail(generics.RetrieveAPIView):
