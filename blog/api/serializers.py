92a01f2686ebfd1bd722c0657ddde596db1d4405
Nasr Elsisi <nasrelsisi00@gmail.com>
2022-07-23 16:59:25 +0000

    Finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,29 +1,7 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
-class PostSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
-        slug_field="value", many=True, queryset=Tag.objects.all()
-    )
-
-    author = serializers.HyperlinkedRelatedField(
-        queryset=User.objects.all(), view_name="api_user_detail", lookup_field="email"
-    )
-
-    class Meta:
-        model = Post
-        fields = "__all__"
-        readonly = ["modified_at", "created_at"]
-    
-#     def validate(self, data):
-#         if not data.get("slug"):
-#             if data["autogenerate_slug"]:
-#                 data["slug"] = slugify(data["title"])
-#             else:
-#                 raise serializers.ValidationError("slug is required if autogenerate_slug is not set")
-#         del data["autogenerate_slug"]
-#         return data
 
 
 # def is_capitalized(value):
@@ -67,11 +45,75 @@ class PostSerializer(serializers.ModelSerializer):
 #             raise serializers.ValidationError("first_name and last_name must be provided together")
 #         return data
 
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        slug_field="value", many=True, queryset=Tag.objects.all()
+    )
+
+    author = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), view_name="api_user_detail", lookup_field="email"
+    )
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+        readonly = ["modified_at", "created_at"]
+    
+#     def validate(self, data):
+#         if not data.get("slug"):
+#             if data["autogenerate_slug"]:
+#                 data["slug"] = slugify(data["title"])
+#             else:
+#                 raise serializers.ValidationError("slug is required if autogenerate_slug is not set")
+#         del data["autogenerate_slug"]
+#         return data
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
+
+
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_data in comments:
+            if comment_data.get("id"):
+                # comment has an ID so was pre-existing
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
+
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
 
 
 
