32fbbe477ab858a1c6ac350008af1b32dcb679e1
Nasr Fakhreldin <101509870+NasrFakhrEldin@users.noreply.github.com>
2022-07-24 02:17:09 +0200

    Update permissions.py

--- blog/api/permissions.py
+++ blog/api/permissions.py
@@ -12,3 +12,10 @@ class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 class IsAdminUserForObject(permissions.IsAdminUser):
     def has_object_permission(self, request, view, obj):
         return bool(request.user and request.user.is_staff)
+    
+    '''
+        This is the same reason our AuthorModifyOrReadOnly inherited from
+        IsAuthenticatedOrReadOnly instead of BasePermission. We want to use
+        IsAuthenticatedOrReadOnly’s has_permission() method and only
+        implement our own has_object_permission() method.
+    '''
