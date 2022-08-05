2a625f4fac15333361893c6b7da66e26840122c2
Nasr Elsisi <nasrelsisi00@gmail.com>
2022-08-05 20:39:29 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -56,6 +56,11 @@ urlpatterns += [
     ),
 
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 
