26dbc0f0acbd4075c4d30246a44e2d30e8c6a439
Nasr Elsisi <nasrelsisi00@gmail.com>
2022-08-15 11:56:59 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,5 +1,25 @@
-const names = []
-let age = 7
-names = "Calvin"
-console.log(names)
-console.log(age)
+
+
+for(let i = 0; i < 10; i+=1){
+  console.log('for loop i: '+i)
+}
+
+let j = 0
+while(j<10){
+  console.log('while loop j: '+j)
+  j += 1
+}
+
+let k = 10
+do{
+  console.log('do while k: ' +k)
+}while(k < 10)
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers.forEach(value=>{
+  console.log('for each value '+value)
+})
+
+const doubled = numbers.map(value => value*2)
+console.log('here are the doubled numbers')
+console.log(doubled)
